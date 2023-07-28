5c41320b3fca9cbd6e2778dc6dd4bc3459ceeceb
James Dickson <jamesstuartdickson@gmail.com>
2023-07-28 12:38:29 +0100

    started new pokemon page and added link to new pokemon in the navbar

--- controllers/pokémon_controller.py
+++ controllers/pokémon_controller.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint, render_template
 from models.pokémon_model import Pokémon
+from models.nurse_model import Nurse
 from app import db
 
 pokémon_blueprint = Blueprint("pokémon", __name__)
@@ -13,3 +14,8 @@ def pokémon():
 def show_pokémon(id):
     single_pokémon = Pokémon.query.get(id)
     return render_template("pokémon/show_pokémon.jinja", single_pokémon=single_pokémon)
+
+@pokémon_blueprint.route("/pokémon/new_pokémon")
+def add_pokémon():
+    nurses = Nurse.query.all()
+    return render_template("pokémon/new_pokémon.jinja")
