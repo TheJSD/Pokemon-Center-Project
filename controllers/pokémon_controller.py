13f2cfd2c4dfa157187a3e4be6150c1d0171f5e5
James Dickson <jamesstuartdickson@gmail.com>
2023-07-28 10:44:51 +0100

    added a page to show a single pokemon and created links in the pokemon template page, updated seed

--- controllers/pokémon_controller.py
+++ controllers/pokémon_controller.py
@@ -8,3 +8,8 @@ pokémon_blueprint = Blueprint("pokémon", __name__)
 def pokémon():
     pokémon = Pokémon.query.all()
     return render_template("pokémon/index.jinja", pokémon=pokémon)
+
+@pokémon_blueprint.route("/pokémon/<id>")
+def show_pokémon(id):
+    single_pokémon = Pokémon.query.get(id)
+    return render_template("pokémon/show_pokémon.jinja", single_pokémon=single_pokémon)
--- seed.py
+++ seed.py
@@ -10,14 +10,15 @@ from flask.cli import with_appcontext
 def seed():
     Nurse.query.delete()
     Pokémon.query.delete()
-    nurse1=Nurse(name="Nurse Joy")
-    nurse2=Nurse(name="Nurse Joy")
-    nurse3=Nurse(name="Nurse Joy")
+    nurse1=Nurse(name="Nurse Joy 1")
+    nurse2=Nurse(name="Nurse Joy 2")
+    nurse3=Nurse(name="Nurse Joy 3")
     nurse4=Nurse(name="Blissey")
 
     pokémon1=Pokémon(species="Pikachu", contact="ashketchum@pokemail.com", dob="12/9/1998")
     pokémon2=Pokémon(species="Onyx", contact="brock@pewtergym.com")
     pokémon3=Pokémon(species="Staryu", contact="misty@ceruleangym.com")
+    pokémon4=Pokémon(species="Geodude", nickname="Rocky", contact="camperliam@pewtergym.com")
 
     db.session.add(nurse1)
     db.session.add(nurse2)
@@ -27,5 +28,6 @@ def seed():
     db.session.add(pokémon1)
     db.session.add(pokémon2)
     db.session.add(pokémon3)
+    db.session.add(pokémon4)
 
     db.session.commit()
