297c8f01c6d3c4bf2a6640e9e0bedba588228730
James Dickson <jamesstuartdickson@gmail.com>
2023-07-29 12:42:56 +0100

    made it possible to register a new nurse, updating details of nurse coming soon

--- controllers/nurse_controller.py
+++ controllers/nurse_controller.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, render_template
+from flask import Flask, Blueprint, render_template, request, redirect
 from models.pokémon_model import Pokémon
 from models.nurse_model import Nurse
 from app import db
@@ -9,3 +9,16 @@ nurses_blueprint=Blueprint("nurses", __name__)
 def nurses():
     nurses = Nurse.query.all()
     return render_template("nurses/index.jinja", nurses=nurses)
+
+
+@nurses_blueprint.route("/nurses/new")
+def register_nurse():
+    return render_template("nurses/new_nurse.jinja")
+
+@nurses_blueprint.route("/nurses", methods=["post"])
+def add_nurse():
+    name = request.form["name"]
+    new_nurse = Nurse(name=name)
+    db.session.add(new_nurse)
+    db.session.commit()
+    return redirect("/nurses")
