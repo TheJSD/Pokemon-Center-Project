b79a777693bb57455fdccfedc9cc56e76a12f6c0
James Dickson <jamesstuartdickson@gmail.com>
2023-07-31 13:35:13 +0100

    can now add a new trainer to db

--- controllers/trainer_controller.py
+++ controllers/trainer_controller.py
@@ -19,3 +19,20 @@ def show_trainer(id):
     assigned_pokémon = Pokémon.query.filter_by(trainer = id)
     assigned_pokémon = empty_returns_null(assigned_pokémon)
     return render_template("/trainers/show_trainer.jinja", trainer = trainer, nurse=nurse, assigned_pokémon=assigned_pokémon)
+
+@trainers_blueprint.route("/trainers/new")
+def register_trainer_page():
+    nurses = Nurse.query.all()
+    return render_template("trainers/new_trainer.jinja", nurses=nurses)
+
+@trainers_blueprint.route("/trainers", methods=["post"])
+def add_trainer():
+    name = request.form['name']
+    contact = request.form['contact']
+    nurse = request.form['nurse']
+    if nurse == "None":
+        nurse = None
+    new_trainer = Trainer(name=name, contact=contact, nurse=nurse)
+    db.session.add(new_trainer)
+    db.session.commit()
+    return redirect("/trainers")
